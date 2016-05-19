e33ceb9c5780e4efe63d87f2cad7cc39caca2acb
Fernando Mayer <fernandomayer@gmail.com>
2016-05-19 19:59:52 -0300

    Try to fix chunk names bug

--- setup_knitr.R
+++ setup_knitr.R
@@ -4,6 +4,7 @@ opts_chunk$set(
     cache = TRUE,
     tidy = FALSE,
     comment = NA,
+    ## collapse = TRUE, ## colapsa chunks em R Markdown
     ## fig.width = 10,
     ## fig.height = 8,
     fig.align = "center",
